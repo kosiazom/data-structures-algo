bedf029b9e565ca92c20c48afb4def38bdc2c6cc
kosiazom <kosiazom@gmail.com>
2020-11-11 10:47:51 -0500

    passed in the console

---
+++ Count_unique.js
@@ -0,0 +1,15 @@
+function countUniqueValues(arr){
+    // add whatever parameters you deem necessary - good luck!
+      let i = 0
+        for(let j = 1; j < arr.length; j++){
+            if(arr[i] !== arr[j]){
+                i++;
+                //so if i doesnt equal j then move onto the next index and set i to equal j
+                arr[i] = arr[j]
+            } 
+            console.log(i, j)
+         }
+         return i + 1
+  }                              
+  countUniqueValues([1,2,3,4,5,6,7,5,5,5,6,7]) 
+                  //        O(n) because we are only looping once                  
