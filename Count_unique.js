1f05d6bdbb2b3196c330f46f2dabc17f3177bd52
kosiazom <kosiazom@gmail.com>
2020-11-18 10:20:52 -0500

    sample problem

--- Count_unique.js
+++ Count_unique.js
@@ -1,5 +1,8 @@
+  //Implement a function called countUniqueValues, which accepts a sorted array,
+    // and counts the unique values in the array. There can be negative numbers in the array but it will always be sorted 
 function countUniqueValues(arr){
     // add whatever parameters you deem necessary - good luck!
+  
       let i = 0
         for(let j = 1; j < arr.length; j++){
             if(arr[i] !== arr[j]){
---
+++ maxSubArray_naive.js
@@ -0,0 +1,39 @@
+// function maxSubarraySum(arr, num) {
+//     if (num > arr.length){
+//         return null;
+//     }
+//     var max = -Infinity; //accting for array with negative numbers
+//     for (let i = 0; i > arr.length - num + 1; i++){     //this is the last place you can start your sum
+//          temp = 0; //temp will store our sum and compare it to max
+//         for (let j = 0; j < num; j++){
+//             temp += arr[i + j];
+//         }
+//         if (temp > max) {
+//             max = temp;
+//         }
+//         console.log(temp, max)
+//     }
+//     return max
+// }
+
+// maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+
+function maxSubarraySum(arr, num) {
+    if ( num > arr.length){
+      return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i ++){
+      temp = 0;
+      for (let j = 0; j < num; j++){
+        temp += arr[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+      console.log(temp, max)
+    }
+    return max;
+  }
+  
+  maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
